621c74833b7321e1b1453b9b819aa400d5742047
Tobias Elod-Zoltan <penumbra.scilicet@gmail.com>
2018-03-25 19:22:08 +0300

    chore(test): consolidate tests

--- test/doctyped.js
+++ test/doctyped.js
@@ -1,33 +1,16 @@
 import test from 'ava';
 import fs from 'fs';
 import path from 'path';
+import sinon from 'sinon';
+import flowParser from 'flow-parser';
 
 import descriptor from './__mocks__/swagger.json';
 import doctyped from '../src/doctyped';
 
 const TEST_PATH_BASE = path.resolve(__dirname, '..', 'tmp');
 
-const cleanDir = (dirname) =>
-  new Promise((resolve) =>
-    fs.readdir(
-      dirname,
-      (err, files) => {
-        if (err && err.errno === -2) {
-          resolve();
-          return;
-        }
-
-        files
-          .filter((f) => f !== '.gitkeep')
-          .forEach((f) => fs.unlink(path.resolve(dirname, f), (err) => {}))
-
-        resolve();
-      }
-    )
-  );
-
 test('generates models', async (t) => {
-  await cleanDir(TEST_PATH_BASE);
+  sinon.stub(process, 'cwd');
 
   const modelNames = Object.keys(descriptor.definitions);
 
@@ -38,7 +21,6 @@ test('generates models', async (t) => {
 
 test('accepts output directory', async (t) => {
   const TEST_PATH = path.resolve(TEST_PATH_BASE, 'definedPath');
-  await cleanDir(TEST_PATH);
 
   const modelNames = Object.keys(descriptor.definitions);
 
@@ -49,4 +31,44 @@ test('accepts output directory', async (t) => {
   await doctyped(path.resolve(__dirname, '__mocks__/swagger.json'), { output: TEST_PATH });
 
   modelNames.forEach((model) => t.truthy(fs.existsSync(`${TEST_PATH}/${model}.js.flow`)));
+});
+
+test.cb('generates flow type', (t) => {
+  const TEST_PATH = path.resolve(TEST_PATH_BASE, 'flow');
+
+  doctyped(path.resolve(__dirname, '__mocks__/swagger.json'), { output: TEST_PATH }).then(() => {
+    fs.readFile(`${TEST_PATH}/Order.js.flow`, (err, response) => {
+      const { body: [body], comments: [{ value }] } = flowParser.parse(response.toString());
+      const { declaration: { id: { name }, right: { properties } }, type } = body;
+      const matchOpts = {
+        id: { annotation: 'NumberTypeAnnotation', type: 'NullableTypeAnnotation' },
+        petId: { annotation: 'NumberTypeAnnotation', type: 'NullableTypeAnnotation' },
+        quantity: { annotation: 'NumberTypeAnnotation', type: 'NullableTypeAnnotation' },
+        shipDate: { annotation: 'GenericTypeAnnotation', type: 'NullableTypeAnnotation' },
+        status: { length: 3, type: 'UnionTypeAnnotation' },
+        complete: { annotation: 'BooleanTypeAnnotation', type: 'NullableTypeAnnotation' }
+      };
+
+      t.truthy(value.includes('@flow'));
+      t.is(type, 'ExportNamedDeclaration');
+      t.is(name, 'Order');
+      t.is(properties.length, 6);
+      properties.forEach(({ key: { name }, value: { type, typeAnnotation, ...value } }) => {
+        const { [name]: match } = matchOpts;
+        t.is(type, match.type);
+        
+        switch(type) {
+          case 'NullableTypeAnnotation':
+            t.is(typeAnnotation.type, match.annotation);
+            return;
+          case 'UnionTypeAnnotation':
+            t.is(value.types.length, match.length);
+            return;
+          default:
+            throw new Error(`Unhandled type '${type}'`);
+        }
+      });
+      t.end();
+    });
+  });
 });

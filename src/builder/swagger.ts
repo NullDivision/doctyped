7d1977c53599fc61b8f5f3c95c1f373560fb4827
Tobias Elod-Zoltan <tobias.elod@gmail.com>
2020-01-04 12:07:03 +0200

    refactor(swagger): fix type issues

--- src/builder/swagger.ts
+++ src/builder/swagger.ts
@@ -1,5 +1,5 @@
 import { SchemaValue, Schema } from '.';
-import { Descriptor, DescriptorValue } from '../reader';
+import { Descriptor, PropertyValue } from '../reader';
 
 interface SwaggerProperty {
   $ref?: string;
@@ -7,58 +7,79 @@ interface SwaggerProperty {
   items: SwaggerProperty;
   type: string;
 }
+interface PropertyTransforms {
+  exportTypes: Array<string> | string | undefined;
+  importTypes: string | undefined;
+  required: boolean;
+  type: string;
+}
 
-const doPropertyTransform = (required) =>
-  (name, { $ref, enum: optsList, items, type }: SwaggerProperty) => {
-    const ucName = `${name[0].toUpperCase()}${name.substr(1)}`;
-    let parsedType = '*';
-    let exportType;
-    let importType;
+const doPropertyTransform = (required: ReadonlyArray<string> | undefined) => (
+  name: string,
+  { $ref, enum: optsList, items, type }: SwaggerProperty
+): PropertyTransforms => {
+  const ucName = `${name[0].toUpperCase()}${name.substr(1)}`;
+  let parsedType = '*';
+  let exportType;
+  let importType: string | undefined;
 
-    switch (type) {
-      case 'integer':
-        parsedType = 'number';
-        break
-      case 'number':
-      case 'string':
-      case 'boolean':
-        parsedType = type;
+  switch (type) {
+    case 'integer':
+      parsedType = 'number';
+      break;
+    case 'number':
+    case 'string':
+    case 'boolean':
+      parsedType = type;
 
-        if (optsList) {
-          parsedType = ucName;
-          exportType = optsList.map((opt) => `'${opt}'`).join('|');
-        }
-        break;
-      case 'array':
-        // eslint-disable-next-line no-case-declarations
-        const subType = doPropertyTransform([])(`${ucName}Opts`, items);
+      if (optsList) {
+        parsedType = ucName;
+        exportType = optsList.map((opt) => `'${opt}'`).join('|');
+      }
+      break;
+    case 'array':
+      // eslint-disable-next-line no-case-declarations
+      const subType = doPropertyTransform([])(`${ucName}Opts`, items);
 
-        parsedType = `Array<${subType.type}>`;
-        exportType = subType.exportTypes;
-        importType = subType.importTypes;
+      parsedType = `Array<${subType.type}>`;
+      exportType = subType.exportTypes;
+      importType = subType.importTypes;
 
-        break;
-      default:
-        if (typeof $ref === 'string') {
-          parsedType = importType = $ref.replace('#/definitions/', '');
-        }
-    }
+      break;
+    default:
+      if (typeof $ref === 'string') {
+        parsedType = importType = $ref.replace('#/definitions/', '');
+      }
+  }
 
-    return {
-      exportTypes: exportType,
-      importTypes: importType,
-      required: !!required && required.includes(name),
-      type: parsedType
-    };
+  return {
+    exportTypes: exportType,
+    importTypes: importType,
+    required: !!required && required.includes(name),
+    type: parsedType
   };
+};
 
-const mergeProperties = (propertyEntries, additionalProperties) => {
-  if (additionalProperties) return [...propertyEntries, ['[string]', additionalProperties]];
+const mergeProperties = (
+  propertyEntries: Array<[string, PropertyValue]>,
+  additionalProperties: PropertyValue | undefined
+): Array<[string, PropertyValue]> => {
+  if (additionalProperties) {
+    return [...propertyEntries, ['[string]', additionalProperties]];
+  }
 
   return propertyEntries;
 };
 
-const mapSwaggerTypes = (name, value: DescriptorValue): SchemaValue => {
+interface DescriptorValue {
+  additionalProperties: PropertyValue;
+  properties: { [key: string]: PropertyValue };
+  required?: ReadonlyArray<string>;
+  type: string;
+  xml: {};
+}
+
+const mapSwaggerTypes = (name: string, value: DescriptorValue): SchemaValue => {
   const { additionalProperties, properties, required } = value;
   const getProperty = doPropertyTransform(required);
   const propertyEntries = Object.entries(properties);
@@ -70,8 +91,10 @@ const mapSwaggerTypes = (name, value: DescriptorValue): SchemaValue => {
   const resolvedProperties = mergedProperties.reduce((acc, [propName, prop]) => {
     if (!(prop instanceof Object)) return acc;
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
     return { ...acc, [propName]: getProperty(propName, prop) };
-  }, {});
+  }, {} as SchemaValue['properties']);
 
   return { name, properties: resolvedProperties };
 };
@@ -82,6 +105,8 @@ export default (definitions: Descriptor['definitions']): Schema => {
   return definitionEntries.map(([name, value]) => {
     if (!(value instanceof Object)) return { name: '', properties: {} };
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
     return mapSwaggerTypes(name, value);
   });
 };
